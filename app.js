e5a37036a27bb7c0e9600008cfbd1e2d673b974a
codrkoaz <codrkoazie@gmail.com>
2024-02-17 16:03:15 -0500

    add more animation to the list

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector(".filter-todo");
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listener
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener("click", filterTodo);
+filterOption.addEventListener('click', filterTodo);
 
 // Function
 function addTodo(event) {
@@ -61,25 +61,24 @@ function deleteCheck(e) {
 
 function filterTodo(e) {
   const todos = todoList.childNodes;
-  console.log(todos)
-  // todos.forEach(function(todo) {
-  //   switch (e.target.value) {
-  //     case "all":
-  //       todo.style.display = "flex";
-  //       break;
-  //     case "completed":
-  //       if (todo.classList.contains("completed")) {
-  //         todo.style.display = "flex";
-  //       } else {
-  //         todo.style.display = "none";
-  //       }
-  //       break;
-  //     case "uncompleted":
-  //       if (!todo.classList.contains("completed")) {
-  //         todo.style.display = "flex";
-  //       } else {
-  //         todo.style.display = "none";
-  //       }
-  //   }
-  // });
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+    }
+  });
 }
