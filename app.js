4690e6ab49d9f133d13b84102963944a97be6cb6
codrkoaz <codrkoazie@gmail.com>
2024-02-17 10:16:08 -0500

    add animation to the todo list

--- app.js
+++ app.js
@@ -7,19 +7,52 @@ const todoList = document.querySelector('.todo-list');
 // Event Listener
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 // Function
 function addTodo(event) {
   event.preventDefault();
-  console.log('hello');
   // todo div
   const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
   // create li
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-items');
   todoDiv.appendChild(newTodo);
-  // Chcecked button
+  // Chcecked Button
   const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  // Trash Button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  // Append To List
+  todoList.appendChild(todoDiv);
+  // Clear Todo Value
+  todoInput.value = '';
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  // DELETE TODO
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    // Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove();
+    });
+  }
+  console.log(deleteCheck);
+
+  // CHECK MARK
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
